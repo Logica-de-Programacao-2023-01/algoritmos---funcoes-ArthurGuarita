f327542f04b57f567bbe325b950786791ee20ab5
ArthurGuarita <arthur.guarita@sempreceub.com>
2023-04-13 20:04:48 -0300

    att

--- algoritmos/algoritmo 5.go
+++ algoritmos/algoritmo 5.go
@@ -1,13 +1,32 @@
 package main
 
-//Crie uma função que receba um slice de inteiros e um valor inteiro, e retorne a posição do primeiro
-//elemento igual ao valor no slice. Caso não encontre, retorne -1.
+import (
+	"fmt"
+)
 
+// Função para encontrar o índice do primeiro elemento igual ao valor no slice
 func encontrarIndex(s []int, n int) int {
-	for i, valor := range s {
-		if s[i] == n {
+	for i, v := range s {
+		if v == n {
 			return i
 		}
 	}
 	return -1
+}
+
+func main() {
+	var s []int
+	var n, num int
+	fmt.Println("Digite o valor do índice: ")
+	fmt.Scan(&n)
+	for {
+		fmt.Print("Digite um número (digite 0 para sair): ")
+		fmt.Scan(&num)
+		if num == 0 {
+			break
+		}
+		s = append(s, num)
+	}
+	result := encontrarIndex(s, n)
+	fmt.Println("Índice do primeiro elemento igual ao valor:", result)
 }
